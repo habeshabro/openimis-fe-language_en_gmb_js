70fc3014e122cfd5c9285df3c533a31b3e5970a9
habeshabro <brouck@gmail.com>
2024-07-20 11:00:44 +0300

    changed some of the labels

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
 }
 
-export const LanguageEnGmbModule = (cfg) => {
+export const LanguageEnModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
